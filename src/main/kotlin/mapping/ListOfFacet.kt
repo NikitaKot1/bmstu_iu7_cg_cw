aa02a2f510dcc6619396c44d27b1312614f47cb3
NikitaKot1 <nikita_kotlyarov2002@mail.ru>
2022-08-16 12:50:53 +0300

    refactor

--- src/main/kotlin/mapping/ListOfFacet.kt
+++ src/main/kotlin/mapping/ListOfFacet.kt
@@ -1,25 +1,19 @@
 package mapping
 
-import javafx.scene.image.Image
 import mapping.objects.SimpleObject
 
-class ListOfFacet (private val obj: SimpleObject){
-    private val image = Image("/drawimage.jpg")
-    public val height = image.height
-    public var map = mutableMapOf<Int, Array<ScanFacetInf>>()
-    init {
-        createMap()
-    }
-    public fun createMap() {
-        map = mutableMapOf<Int, Array<ScanFacetInf>>()
+object ListOfFacet{
+    public fun createMap(obj: SimpleObject) : Map<Int, Array<ScanFacetInf>>{
+        val map = mutableMapOf<Int, Array<ScanFacetInf>>()
         for (facet in obj.facets) {
             val inf = ScanFacetInf(facet)
             if (map.containsKey(inf.yScanMin.toInt())) {
-                var arr = map[inf.yScanMin.toInt()]
+                val arr = map[inf.yScanMin.toInt()]
                 map[inf.yScanMin.toInt()] = arr!! + arrayOf(inf)
             } else {
                 map[inf.yScanMin.toInt()] = arrayOf(inf)
             }
         }
+        return map
     }
 }
--- src/main/kotlin/mapping/StringScan.kt
+++ src/main/kotlin/mapping/StringScan.kt
@@ -1,17 +1,12 @@
 package mapping
 
 import javafx.scene.image.Image
-import mapping.objects.Dot
 import mapping.objects.SimpleObject
 
 class StringScan (obj: SimpleObject, matr: RotateMatrix) {
     private val image = Image("/drawimage.jpg")
     public val width = image.width
     public val newObj: SimpleObject = Transformator.transform(obj, matr)
-
-    public fun preparation () {
-        for (facet in newObj.facets) {
-
-        }
-    }
+    public val map = ListOfFacet.createMap(obj)
+        
 }
